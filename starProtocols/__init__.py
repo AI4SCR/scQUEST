843926611118d9a2b7f1bea7948c37daf20537aa
Adriano Martinelli <art@zurich.ibm.com>
2022-01-13 17:52:53 -0500

    refactoring [skip ci]

--- starProtocols/__init__.py
+++ starProtocols/__init__.py
@@ -1,3 +1,6 @@
 """Module initialization."""
 
 __version__ = "0.1.0"
+
+from .abnormality import Abnormality
+from .individuality import Individuality
---
+++ starProtocols/abnormality.py
@@ -0,0 +1,3 @@
+
+class Abnormality:
+    pass
