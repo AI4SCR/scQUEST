6b614acabba21ea4c4501f5f9dd6e03bda4d60df
Adriano Martinelli <art@zurich.ibm.com>
2022-01-30 15:02:09 -0500

    abstract classes for Estimators, LitModules

--- starProtocols/data.py
+++ starProtocols/data.py
@@ -1,18 +1,27 @@
 import torch
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 
-from anndata import AnnData
+import torch
+
+from torch.utils.data import random_split, DataLoader
+
+import pytorch_lightning as pl
+
+from typing import Optional, Iterable
+from .preprocessing import Preprocessor
+
+TRAIN_DATALOADERS = EVAL_DATALOADERS = DataLoader
+
 from scipy.sparse import issparse
+from anndata import AnnData
 
-from typing import Optional
 
+class AnnDatasetClf(Dataset):
 
-class AnnDataset(Dataset):
-
-    def __init__(self, ad: AnnData, target: Optional[str] = None, layer=None,
+    def __init__(self, ad: AnnData, target: Optional[str], layer=None,
                  encode_targets_one_hot: bool = True) -> None:
-        super(AnnDataset, self).__init__()
+        super(AnnDatasetClf, self).__init__()
         self.ad = ad
         self.target = target
         self.layer = layer
@@ -28,3 +37,96 @@ class AnnDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self.data)
+
+
+class AnnDatasetAE(Dataset):
+
+    def __init__(self, ad: AnnData, layer=None, *args, **kwargs) -> None:
+        super(AnnDatasetAE, self).__init__()
+        self.ad = ad
+        self.layer = layer
+
+        data = ad.X if layer is None else ad.layers[layer]
+        self.data = torch.tensor(data.A).float() if issparse(data) else torch.tensor(data).float()
+
+    def __getitem__(self, item) -> (torch.TensorType, torch.TensorType):
+        return self.data[item], self.data[item]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+
+class AnnDataModule(pl.LightningDataModule):
+
+    def __init__(self, ad: AnnData, target: str,
+                 ad_dataset_cls,
+                 layer: Optional[str] = None,
+                 preprocessing: Optional[Iterable[Preprocessor]] = None,
+                 test_size: float = 0.25, validation_size: float = 1 / 3,
+                 batch_size: int = 32, seed: int = 42):
+        super(AnnDataModule, self).__init__()
+
+        self.ad = ad
+        self.target = target
+        self.layer = layer
+        self.preprocessing = preprocessing
+        self.ad_dataset_cls = ad_dataset_cls
+        self.test_size = test_size
+        self.validation_size = validation_size
+        self.batch_size = batch_size
+        self.seed = seed
+
+        self.dataset = ad_dataset_cls(ad, target=target, layer=layer)
+
+        # datasets
+        self.train = None
+        self.fit = None
+        self.val = None
+        self.test = None
+
+    # TODO: How should we preprocess the data? Preprocessing should be part of the DataModule to keep everything related to data together
+    #   however, in its current form we have to implement the Processor for torch Datasets and Subsets.
+    def setup(self, stage: Optional[str] = None) -> None:
+        dataset = self.dataset
+        n_test = int(len(dataset) * self.test_size)
+        n_train = len(dataset) - n_test
+
+        self.train, self.test = random_split(dataset, [n_train, n_test],
+                                             generator=torch.Generator().manual_seed(self.seed))
+        self._fit_preprocessors()  # TODO: Should we fit the preprocessors on the whole train or on fit,val individually?
+
+        if stage in ('fit', None):
+            train = self.train
+            if self.preprocessing:
+                for pp in self.preprocessing:
+                    self.train.dataset.data = pp.transform(self.train)
+
+            n_val = int(len(train) * self.validation_size)
+            n_fit = len(train) - n_val
+
+            self.fit, self.val = random_split(train, [n_fit, n_val], generator=torch.Generator().manual_seed(self.seed))
+
+        if stage in ('test', None):
+            if self.preprocessing:
+                for pp in self.preprocessing:
+                    self.test.dataset.data = pp.transform(self.test)
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(self.fit, batch_size=self.batch_size)
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.val, batch_size=self.batch_size)
+
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test, batch_size=self.batch_size)
+
+    def _fit_preprocessors(self):
+        if self.preprocessing:
+            for pp in self.preprocessing:
+                pp.fit(self.train)
+
+    def __getitem__(self, item):
+        return self.dataset[item]
+
+    def __len__(self):
+        return len(self.dataset)
--- starProtocols/utils.py
+++ starProtocols/utils.py
@@ -1,5 +1,192 @@
 from pandas.api.types import CategoricalDtype
+from itertools import tee
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+import torchmetrics
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.utilities.types import STEP_OUTPUT
+
+from typing import Iterable, Optional, List, Union
+from .preprocessing import Preprocessor
+
+TRAIN_DATALOADERS = EVAL_DATALOADERS = DataLoader
+
+from scipy.sparse import issparse
+from anndata import AnnData
+
+from .data import AnnDataModule
+
+
+# NOTE: pairwise is distributed with `itertool` in python>=3.10
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
 def isCategorical(x):
     return isinstance(x, CategoricalDtype)
+
+
+class LitModule(pl.LightningModule):
+
+    def __init__(self, model: nn.Module,
+                 loss_fn,
+                 metrics: Iterable[torchmetrics.Metric],
+                 learning_rate=1e-3):
+        super(LitModule, self).__init__()
+
+        self.model = model
+        self.loss = loss_fn
+
+        # we create for each metric a own attribute to use be able to automatic logging with lightning
+        self.metric_attrs = []
+        for metric in metrics:
+            attr = f'metric_{metric._get_name()}'.lower()
+            setattr(self, attr, metric)
+            self.metric_attrs.append(attr)
+
+        self.learning_rate = learning_rate
+
+    def forward(self, x) -> int:
+        raise NotImplementedError()
+
+    def training_step(self, batch, batch_idx) -> STEP_OUTPUT:
+        x, y = batch
+        yhat = self.model(x)
+        loss = self.loss(yhat, y)
+        self.log('fit_loss', loss.detach())
+        self.log_metrics('train', y, yhat)
+        return loss
+
+    def validation_step(self, batch, batch_idx) -> Optional[STEP_OUTPUT]:
+        x, y = batch
+        yhat = self.model(x)
+        loss = self.loss(yhat, y)
+        self.log('val_loss', loss.detach())
+        self.log_metrics('val', y, yhat)
+        return loss
+
+    def test_step(self, batch, batch_idx) -> Optional[STEP_OUTPUT]:
+        x, y = batch
+        yhat = self.model(x)
+        loss = self.loss(yhat, y)
+        self.log('test_loss', loss.detach())
+        self.log_metrics('test', y, yhat)
+
+        return loss
+
+    def configure_optimizers(self):
+        # https://pytorch-lightning.readthedocs.io/en/latest/api/pytorch_lightning.core.lightning.html#pytorch_lightning.core.lightning.LightningModule.configure_optimizers
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+    def log_metrics(self, step, y, yhat):
+        # https://pytorch-lightning.readthedocs.io/en/stable/extensions/logging.html
+        true_cls, pred_cls = y.argmax(axis=1), yhat.argmax(axis=1)
+
+        for metric in self.metric_attrs:
+            m = getattr(self, metric)
+            m(pred_cls, true_cls)
+            self.log(f'{step}_{metric}', m)
+
+
+class Estimator:
+
+    def __init__(self, model: Optional[nn.Module] = None,
+                 loss_fn: Optional = None,
+                 metrics: Optional = None,
+                 ):
+
+        self.ad = None
+        self.target = None
+        self.datamodule = None
+        self.trainer = None
+
+        self.loss_fn = loss_fn if loss_fn else self._default_loss()
+        self.metrics = metrics if metrics else self._default_metric()
+        LM = self._default_litModule()
+
+        if model is None:
+            self.model = LM(model=self._default_model(),
+                            loss_fn=self.loss_fn,
+                            metrics=self.metrics)
+        else:
+            if isinstance(model, pl.LightningModule):
+                self.model = model
+            elif isinstance(model, nn.Module):
+                self.model = LM(model=model,
+                                loss_fn=self.loss_fn,
+                                metrics=self.metrics)
+            else:
+                raise TypeError(f'Model should be of type LightningModule or nn.Module not {type(model)}')
+
+    def fit(self, ad: Optional[AnnData] = None, target: Optional[str] = None,
+            datamodule: Optional[pl.LightningDataModule] = None,
+            preprocessing: Optional[List[Preprocessor]] = None,
+            early_stopping: Union[bool, EarlyStopping] = True,
+            max_epochs: int = 100,
+            callbacks: list = None):
+
+        callbacks = [] if callbacks is None else callbacks
+        self.target = 'target' if target is None else target
+
+        if datamodule is None:
+            self.datamodule = AnnDataModule(ad, target,
+                                            ad_dataset_cls=self._configure_anndata_class(),
+                                            preprocessing=preprocessing)
+        else:
+            self.datamodule = datamodule
+
+        if early_stopping:
+            if isinstance(early_stopping, EarlyStopping):
+                callbacks.append(early_stopping)
+            else:
+                callbacks.append(EarlyStopping(monitor='val_loss',
+                                               mode='min',
+                                               min_delta=1e-3,
+                                               patience=0))
+
+        self.trainer = pl.Trainer(logger=False,
+                                  enable_checkpointing=False,
+                                  max_epochs=max_epochs,
+                                  callbacks=callbacks,
+                                  reload_dataloaders_every_epoch=False)
+        self.trainer.fit(model=self.model, datamodule=self.datamodule)
+        self.trainer.test(model=self.model, datamodule=self.datamodule)
+
+    def predict(self, ad: AnnData, layer: Optional[str] = None, inplace=True):
+        self.ad = ad if inplace else ad.copy()
+
+        X = ad.X if layer is None else ad.layers[layer]
+        X = X.A if issparse(X) else X
+        X = torch.tensor(X).float()
+
+        self._predict(X)
+
+        if not inplace:
+            return ad
+
+    def _default_model(self) -> nn.Module:
+        raise NotImplementedError()
+
+    def _configure_anndata_class(self) -> nn.Module:
+        raise NotImplementedError()
+
+    def _default_loss(self):
+        raise NotImplementedError()
+
+    def _default_metric(self):
+        raise NotImplementedError()
+
+    def _default_litModule(self):
+        raise NotImplementedError()
+
+    def _predict(self, X, model, ad):
+        raise NotImplementedError()
