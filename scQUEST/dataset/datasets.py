88d968c709e7e8b19183982f076562031552cbb9
Adriano Martinelli <art@zurich.ibm.com>
2022-03-23 16:46:41 -0400

    update dataset links

--- scQUEST/dataset/datasets.py
+++ scQUEST/dataset/datasets.py
@@ -5,7 +5,7 @@ def breastCancerAtlas(force_load=False):
     """Flow cytometry dataset with annotated epithelial cells in `['celltype', 'celltype_class']` of `ad.obs`. Locally stored at ~/.scQUEST"""
     return DataSet(
         name='breastCancerAtlas',
-        url='https://figshare.com/ndownloader/files/34432184',
+        url='https://figshare.com/ndownloader/files/34486574',
         doc_header='Processed breast cancer atlas from https://doi.org/10.1016/j.cell.2019.03.005',
         force_load=force_load)()
 
@@ -14,6 +14,6 @@ def breastCancerAtlasRaw(force_load=False):
     """Raw flow cytometry dataset. Locally stored at ~/.scQUEST"""
     return DataSet(
         name='breastCancerAtlasRaw',
-        url='https://figshare.com/ndownloader/files/34437182',
+        url='https://figshare.com/ndownloader/files/34488644',
         doc_header='Raw breast cancer atlas from https://doi.org/10.1016/j.cell.2019.03.005',
         force_load=force_load)()
