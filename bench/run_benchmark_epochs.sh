0cc4b80e99ad5e469381c657bc1f6ce60d143f2e
Adriano Martinelli <art@zurich.ibm.com>
2022-03-23 11:35:04 -0400

    stop tracking /bench and ignore various files

--- bench/run_benchmark_epochs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#conda activate scQUEST
-python benchmark.py 'torch' 10000 small 250 256 10
-python benchmark.py 'torch' 10000 large 250 256 10
-python benchmark.py 'light' 10000 small 250 256 10
-python benchmark.py 'light' 10000 large 250 256 10
-
