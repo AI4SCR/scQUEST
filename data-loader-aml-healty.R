4547f2b519f93822ad48c26ba80962971bf8fdfc
Adriano Martinelli <art@zurich.ibm.com>
2022-05-25 15:18:23 +0200

    adjust dataloader to use figshare files

--- data-loader-aml-healty.R
+++ data-loader-aml-healty.R
@@ -1,12 +1,21 @@
 # load packages
-setwd("~/Documents/projects/scQUEST")
-pkgs = readr::read_lines('fcs-requirements.txt')
+pkgs = c('flowCore',
+         'anndata',
+         'tidyverse',
+         'magrittr',
+         'httr')
 for(i in pkgs) suppressPackageStartupMessages(library(i, character.only = TRUE))
 
 # define the python interpreter to use
 reticulate::use_python('/usr/local/Caskroom/miniconda/base/envs/r-reticulate-env/bin/python')
 
-# helper
+# request for figshare api
+project_id = 140062
+articel_id = 19867021
+PATH = '~/.scQUEST/Levine_CyTOF_AML_AnnotatedCellTypes/'
+REQUEST = paste0('https://api.figshare.com/v2/articles/', articel_id, '/files')
+
+# helpers
 mapper = function(key, df){
   mapping= list()
   cols = colnames(df)
@@ -19,20 +28,39 @@ mapper = function(key, df){
   return (mapping)
 }
 
-# files
-root = '/Users/art/Library/CloudStorage/Box-Box/STAR_protocol/Revision/Datasets'
-root = '/Users/art/Downloads/Datasets'
-level = 'Levine_CyTOF_AML_Healthy_vs_AML'
+download_files = function(path = PATH, request = REQUEST){
+  path = path.expand(path)
+  dir.create(path, showWarnings = TRUE, recursive = TRUE, mode = "0777")
+  
+  resp = GET(request)
+  stop_for_status(resp)
+  
+  files = content(resp, 'parsed')
+  files = map(files, function(x){
+    url = x$download_url
+    fpath = paste0(path, x$name)
+    if(!file.exists(fpath)){
+      download.file(url, fpath)
+    }
+    return(c(url, fpath))
+  })
+}
 
-files = dir(paste(root, level, sep='/'))
+# download files
+files = download_files(PATH, REQUEST)
+files = unlist(map(files, ~.x[2]))
 files.fcs = files[grepl('.fcs$', files)]
+files.csv = files[grepl('.csv$', files)]
+
+files.sampleID = files.csv[grepl('sampleID', files.csv)]
+files.channels = files.csv[grepl('channels', files.csv)]
 
 # mappings
-rename.sample = readr::read_csv(paste(root, level, 'Levine_Healthy_vs_AML_sampleID.csv', sep='/'))
+rename.sample = readr::read_csv(files.sampleID)
 rename.sample  %<>% rename(sample_id = sample_ID)
 map.sample = mapper('sample_id', rename.sample)
 
-rename.channel = readr::read_csv(paste(root, level, 'Levine_Healthy_vs_AML_channels.csv', sep='/'))
+rename.channel = readr::read_csv(files.channels)
 map.channel = mapper('channel', rename.channel)
 
 # load FCS file
@@ -44,18 +72,18 @@ fcs.header = list()
 f.count = 0
 for(f in files.fcs){
   f.count = f.count + 1
-  cat(f.count, '/', length(files.fcs), ' reading in file ', f, '\n')
-  file.name = paste(root, level, f, sep='/')
+  f.base =  basename(f)
+  cat(f.count, '/', length(files.fcs), ' reading in file ', f.base, '\n')
   
-  header = read.FCSheader(file.name)
+  header = read.FCSheader(f)
   header.tbl = tibble(keyword = names(header[[1]]), value=header[[1]])
-
-  fcs <- read.FCS(file.name)
+  
+  fcs <- read.FCS(f)
   x = exprs(fcs)
   
-  obs = list(fcs_file = f)
+  obs = list(fcs_file = f.base)
   for(i in names(map.sample)){
-    obs[[i]] = map.sample[[i]][[f]]
+    obs[[i]] = map.sample[[i]][[f.base]]
   }
   
   obs = do.call(tibble, obs)
@@ -70,7 +98,7 @@ for(f in files.fcs){
   else stopifnot(all(VAR == var))
   
   stopifnot(!f %in% names(fcs.header)) 
-  fcs.header[[f]] = header.tbl
+  fcs.header[[f.base]] = header.tbl
 }
 
 # tidy data
@@ -90,5 +118,5 @@ ad = AnnData(
   )
 )
 
-#saveRDS(ad, paste(root, level, 'ad.rds', sep='/'))
-anndata::write_h5ad(ad, paste(root, level, 'ad_healty_aml.h5ad', sep='/'))
+path.output = path.expand(paste0(PATH, 'ad_healty_aml.h5ad'))
+anndata::write_h5ad(ad, path.output)
